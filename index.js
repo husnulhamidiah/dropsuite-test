5fbd72baa4bac8fc7fb6b7b99fb8c6da012146f7
Husnul Anwari <husnulhamidiah@gmail.com>
2019-10-05 09:53:36 +0700

    Get all hashes

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('hello world')
+import { execSync } from 'child_process'
+
+const dirPath = process.argv.slice(2)
+if (!dirPath.length) process.exit(1)
+
+const shasums = execSync(`find ${dirPath} -type f -exec sha1sum {} \\;`)
+const results = shasums.toString()
+  .split('\n')
+  .filter(Boolean)
+  .map(item => item.substring(0, 40))
+
+console.log(results)
